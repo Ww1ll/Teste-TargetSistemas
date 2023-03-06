60d5fbf59bc44c2293ac59103345543c42b08f55
William <109118303+Ww1ll@users.noreply.github.com>
2023-03-06 11:08:15 -0300

    Add files via upload

---
+++ 01-verificarSoma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	
+	int indice = 13, soma = 0, k = 0;
+
+		while(k < indice){
+			k += 1;
+			soma += k;
+		}
+
+	cout<<"\nSoma:"<<soma;
+	
+}
